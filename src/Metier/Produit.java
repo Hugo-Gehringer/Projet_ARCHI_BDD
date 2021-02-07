8138e7990b6b9d449a5291b7c4314cbc3a3c94e2
N <wiicke@live.fr>
2021-02-07 15:32:28 +0100

    Classe produit

--- src/Metier/Produit.java
+++ src/Metier/Produit.java
@@ -1,16 +1,61 @@
 package Metier;
 
-import java.util.ArrayList;
+public class Produit implements I_Produit {
 
-public class Produit {
+    int quantiteStock = 0;
+    String nom = null;
+    double prixUnitaireHT = 0;
+    double tauxTVA = 0.2;
 
-    private String nom;
 
-    public Produit(String nom, double prix, int qte) {
-
+    @Override //Je considère que cette méthode doit ajouter une quantite au stock, et si c'est fait elle retourne TRUE
+    public boolean ajouter(int qteAchetee) {
+        boolean ajout = false;
+        int laQt = this.quantiteStock;
+        this.quantiteStock = this.quantiteStock + qteAchetee;
+        if(laQt != quantiteStock){
+            ajout = true;
+        }
+        return ajout;
     }
 
+    @Override //Je considère que cette méthode doit retirer une quantite au stock, et si c'est fait elle retourne TRUE
+    public boolean enlever(int qteVendue) {
+        boolean enleve = false;
+        int laQt = this.quantiteStock;
+        if(laQt >= qteVendue) {
+            this.quantiteStock = this.quantiteStock - qteVendue;
+            if (laQt != quantiteStock) {
+                enleve = true;
+            }
+        }
+        return enleve;
+    }
+
+    @Override
     public String getNom() {
-        return nom;
+        return this.nom;
     }
+
+    @Override
+    public int getQuantite() {
+        return this.quantiteStock;
+    }
+
+    @Override
+    public double getPrixUnitaireHT() {
+        return this.prixUnitaireHT;
+    }
+
+    @Override
+    public double getPrixUnitaireTTC() {
+        return this.prixUnitaireHT * (1 + this.tauxTVA);
+    }
+
+    @Override
+    public double getPrixStockTTC() {
+        return this.quantiteStock * (this.prixUnitaireHT * (1 + this.tauxTVA));
+    }
+
+
 }
