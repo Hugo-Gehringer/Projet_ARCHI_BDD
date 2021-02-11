f97668e3094e2925727939a1c013a2241b0bffb7
N <wiicke@live.fr>
2021-02-11 18:18:49 +0100

    Fen err n°2

--- src/Application/AjoutSuppressionController.java
+++ src/Application/AjoutSuppressionController.java
@@ -6,22 +6,23 @@ import static Presentation.FenetrePrincipale.catalogueF;
 public class AjoutSuppressionController {
 
     public static boolean creerProduit(String nom,String prixHT,String qteStock){
-        boolean par = true;
         boolean creation = false;
         int qteS = 0;
         Double prix = 0.00;
+
+        if(nom.equals(null) || nom == null || nom.trim().isEmpty()) {
+            return false;
+        }
+
         try {
             qteS = Integer.parseInt(qteStock);
             prix = Double.parseDouble(prixHT);
         } catch (final NumberFormatException e) {
             return false;
         }
-        if(par){
-            Produit produit = new Produit(nom, prix, qteS);
-            catalogueF.addProduit(produit);
-            creation = true;
-        }
-        return creation;
+
+        Produit produit = new Produit(nom, prix, qteS);
+        return catalogueF.addProduit(produit);
     }
 
     public static boolean removeProduit(String Nom){
